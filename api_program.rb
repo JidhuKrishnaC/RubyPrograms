6ab243afc4773892d75ae1f62055257d12553aa8
jkrishna.c <jkrishna.c@gmail.com>
2022-08-16 09:04:05 +0530

    api progrm

---
+++ api_program.rb
@@ -0,0 +1,10 @@
+# Write a program to make api call to https://api.covid19api.com/summary and print the results. Use exception handling to handle api errors. If API call is successfull display results else display a message that call is not successfull
+
+require 'httparty'
+
+begin
+    res = HTTParty.get('https://api.covid19api.com/summary')
+    puts res.body if res.code == 200
+rescue
+    puts "Call wasnt successful!!"
+end
